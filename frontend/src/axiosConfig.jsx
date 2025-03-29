8ac4022e5ecc7986d9d69f9b618fa349b3bf1f5e
Seethal <r.seethal1998@gmail.com>
2025-03-29 23:42:12 +1000

    updated ip in axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,17 +3,11 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://13.211.170.208:5001',
   headers: {
     Authorization: token ? `Bearer ${token}` : '',
   },
 });
 
-// richu
-// const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:9080', // local
-//   //baseURL: 'http://3.26.96.188:5001', // live
-//   headers: { 'Content-Type': 'application/json' },
-// });
 
 export default axiosInstance;
