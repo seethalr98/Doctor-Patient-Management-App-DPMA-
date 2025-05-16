d7f9ef95243aaecaa10db46efb784cab881503d6
Seethal <r.seethal1998@gmail.com>
2025-05-16 22:25:23 +1000

    latest

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://13.211.170.208:5001',
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://13.211.170.208:5001',
+  // baseURL: 'http://localhost:5001',
   headers: {
     Authorization: token ? `Bearer ${token}` : '',
   },
